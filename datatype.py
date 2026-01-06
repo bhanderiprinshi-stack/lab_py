a75b9171ffdbfb0502136bb4e1581ed347f72137
bhanderiprinshi-stack <bhanderibharti5@gmail.com>
2026-01-06 08:14:56 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,46 @@
+#string data type
+x="hello world!"
+print(x)
+#int data type
+x=20
+print(x)
+a=20
+b=30
+print(a+b)
+#float datatype
+x=20.5
+print(x)
+#complex data type
+x=1j
+print(x)
+#list data type
+x=["apple","banana","cherry"]
+print(x)
+#tuple data type
+x=("aplle","orange","rasberry")
+print(x)
+#bool data type
+x="true"
+print(x)
+#dict data type
+x={"name":"john","age":36}
+print(x)
+#range data type
+x=range(6)
+print(x)
+#bytearray data type
+x=bytearray(5)
+print(x)
+#byte data type
+x=b"hello"
+print(x)
+#set data type
+x={"prinshi","tritha","krisha"}
+print(x)
+#memoryview data type
+x=memoryview(bytes(5))
+print(x)
+#frozenset data type
+x=frozenset({"apple","banana","cherry"})
+print(x)
+
