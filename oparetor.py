dba4206a4dd874fea46b293991e395fad3a6c30d
Bhanderi prinshi <bhanderibharti5@gmail.com>
2026-01-27 08:10:04 +0530

    Add files via upload

---
+++ oparetor.py
@@ -0,0 +1,55 @@
+#1.arithmetic operators
+a=5
+b=10
+print(a+b)
+print(a-b)
+print(a/b)
+
+#2.assignment oparetors
+x=10
+print(x)
+
+#3.unary minus oparetors
+a=5
+print(-a)
+
+#4.relational oparetors
+p=10
+q=5
+print(p>q)
+print(p==q)
+
+#5.logical oparetors
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+#or,and,not numeric
+x=10
+y=15
+print(x and y)
+print(x or y)
+print(not x)
+
+#6.boolean oparetor
+is_pass=True
+print(is_pass)
+
+#7.bitwise oparetor
+a=5
+b=3
+print(a&b)
+print(a/b)
+
+#8.membership oparetors
+numbers=[1,2,3,4]
+print(2 in numbers)
+print(5 is not numbers)
+
+#9.identity oparetors
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
