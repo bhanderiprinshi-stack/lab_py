1f67e2325540dc124e0f77f310b1c86cb33dc1ba
Bhanderi prinshi <bhanderibharti5@gmail.com>
2026-01-31 09:19:51 +0530

    Add files via upload

---
+++ io.py
@@ -0,0 +1,22 @@
+#input/output statement
+print("hello world!")
+
+name=input(('enter your name:'))
+print("hello",name)
+
+num=input(('enter you num:'))
+print("hello",num)
+
+num=input(('enter your number:'))
+print("float number",num)
+
+first=input(('enter first word:'))
+second=input(('enter second word:'))
+print("youn tyoed",first,second)
+
+print("a","b","c")
+print("a","b","c",sep="-")
+
+name=input(('enter your name:'))
+marks=input(('enter your marks:'))
+print("your name and marks",name,marks)
