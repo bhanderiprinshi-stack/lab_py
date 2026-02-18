1c98c55b720669789c609c12fe57bf832ee23b0e
Bhanderi prinshi <bhanderibharti5@gmail.com>
2026-02-18 12:30:45 +0530

    Add files via upload

---
+++ if.py
@@ -0,0 +1,22 @@
+#positive negative or zero
+num=int(input("enter a value:"))
+if num>0:
+    print("the num is positive")
+elif num<0:
+    print("the num is negative")   
+else:
+    print("zero") 
+    
+#odd even
+num=int(input("enter the number:"))
+if num % 2 == 0:
+    print("the num is even") 
+else:
+    print("the num is odd") 
+    
+#eligible foe vote
+age=18
+if age>=18:
+    print("you are aligible to vote")
+    
+ 
